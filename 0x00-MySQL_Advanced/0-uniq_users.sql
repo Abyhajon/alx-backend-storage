47013bc10e959d0f73618c09717d1024a0d0ddef
Abyhajon <joedabongo01@gmail.com>
2023-08-11 13:02:25 +0300

    corrected table name

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,6 +1,5 @@
 -- create table users --
-
-CREATE TABLE IF NOT EXISTS user (
+CREATE TABLE IF NOT EXISTS users (
     id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
     email VARCHAR(255) NOT NULL UNIQUE,
     name VARCHAR(255)
