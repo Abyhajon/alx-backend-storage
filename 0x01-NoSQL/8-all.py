424f0160718c58af7a6fbd5b26f4e1f2d3a965fa
Abyhajon <joedabongo01@gmail.com>
2023-08-15 21:29:39 +0300

    task 8

---
+++ 0x01-NoSQL/8-all.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+""" MongoDB Operations with python """
+
+def list_all(mongo_collection):
+    documents = db.mongo_collection.find()
+    if documents.count() == 0:
+        return []
+
+    return documents
