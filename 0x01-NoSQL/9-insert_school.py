15d0a22d34f0b8bc9dbb2e8ad58c0ef3de5e680e
Abyhajon <joedabongo01@gmail.com>
2023-08-15 21:42:12 +0300

    task 9

---
+++ 0x01-NoSQL/9-insert_school.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+"""MongoDB Operations with python using pymongo """
+
+def insert_school(mongo_collection, **kwargs):
+    """ inserts ne documents in collection """
+    return db.mongo_collection.insert(kwargs)
