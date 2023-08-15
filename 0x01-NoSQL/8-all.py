9ad6058ea810e03c632e02477b328536734de970
Abyhajon <joedabongo01@gmail.com>
2023-08-15 21:44:05 +0300

    task 8 added extra line after comments

--- 0x01-NoSQL/8-all.py
+++ 0x01-NoSQL/8-all.py
@@ -1,9 +1,12 @@
 #!/usr/bin/env python3
 """ MongoDB Operations with python """
 
+
 def list_all(mongo_collection):
     """ lists documents in a collection """
     documents = db.mongo_collection.find()
+
     if documents.count() == 0:
         return []
+
     return documents
