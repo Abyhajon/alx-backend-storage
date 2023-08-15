c9a60820ca0755e12d3cad8795fbf4032dc7ffb4
Abyhajon <joedabongo01@gmail.com>
2023-08-15 21:56:10 +0300

    task 9

--- 0x01-NoSQL/9-insert_school.py
+++ 0x01-NoSQL/9-insert_school.py
@@ -4,4 +4,4 @@
 
 def insert_school(mongo_collection, **kwargs):
     """ inserts ne documents in collection """
-    return db.mongo_collection.insert(kwargs)
+    return db.mongo_collection.insert_one(kwargs).inserted_id
